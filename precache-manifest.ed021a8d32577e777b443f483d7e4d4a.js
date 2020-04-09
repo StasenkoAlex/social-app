63f8ea39fc4ce23c23208a0b7e156094f30ed5a3
AlexandraSt <adubovika@gmail.com>
2020-04-09 15:17:17 +0300

    Updates

---
+++ precache-manifest.ed021a8d32577e777b443f483d7e4d4a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7c437129b421b4df999ce643d15b55",
+    "url": "/social-app/index.html"
+  },
+  {
+    "revision": "40a0482e10e3f39e29f9",
+    "url": "/social-app/static/css/2.4f2c5198.chunk.css"
+  },
+  {
+    "revision": "f93471aa94e0f82ff0b0",
+    "url": "/social-app/static/css/main.0d096e92.chunk.css"
+  },
+  {
+    "revision": "40a0482e10e3f39e29f9",
+    "url": "/social-app/static/js/2.5a6dd076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-app/static/js/2.5a6dd076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93471aa94e0f82ff0b0",
+    "url": "/social-app/static/js/main.7437e817.chunk.js"
+  },
+  {
+    "revision": "5330be14af0234af83b7",
+    "url": "/social-app/static/js/runtime-main.d2ea847e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/social-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/social-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/social-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/social-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/social-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "180d54b892b5af9f92b36d3c48737d34",
+    "url": "/social-app/static/media/logo.180d54b8.svg"
+  }
+]);
